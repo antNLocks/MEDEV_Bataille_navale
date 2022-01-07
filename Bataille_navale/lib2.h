c802b865481d647c6545577fcba4f313a99ed087
Antoine LORENTZ <antn.lorentz@gmail.com>
2022-01-07 12:02:06 +0100

    Test d'OpenGL. Fonctionne sous Linux

---
+++ Bataille_navale/lib2.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// library to test
+int add2(int const p_a, int const p_b);
--- Bataille_navale/main.cpp
+++ Bataille_navale/main.cpp
@@ -1,9 +1,86 @@
+// OpenGLHelloWorld.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+//#include <Windows.h>
 #include <iostream>
 
-#include "lib.h"
+#include <GL/gl.h>
+#include <GL/glut.h>
 
-// entry point of the main project
-int main(int argc, char **argv)
-{
-    std::cout << add(5, 3) << std::endl;
+GLvoid affichage() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(0.5f, -0.5f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(0.5f, 0.5f);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(-0.5f, 0.5f);
+
+    glEnd();
+
+    
+
+    glEnd();
+
+    glFlush();
 }
+
+GLvoid damier()
+{
+    return GLvoid();
+}
+
+GLvoid clavier(unsigned char touche, int x, int y) {
+    switch (touche) {
+    case 'p':
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); //Plein
+        break;
+    case 'f':
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //Fil de fer
+        break;
+    case 's':
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT); //Sommets
+        break;
+    case 'q':
+    case 27 :
+        exit(0);
+        break;
+    }
+
+    glutPostRedisplay(); //Reaffiche la scene
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); //Init de glut
+    glutInitDisplayMode(GLUT_RGB); //Choix du mode d'affichage
+    glutInitWindowPosition(200, 200); //Position initaile de la fenêtre
+    glutInitWindowSize(700, 700); //Taille initiale de la fenêtre
+    glutCreateWindow("OpenGL Hello World"); //Creation de la fenêtre GLUT
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //Couleur effacement buffer
+    
+    //Fonction de callbacks
+    glutDisplayFunc(affichage);
+    glutKeyboardFunc(clavier);
+
+    glutMainLoop();
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+// Astuces pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
