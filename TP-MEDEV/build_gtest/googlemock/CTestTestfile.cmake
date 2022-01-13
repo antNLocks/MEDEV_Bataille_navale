9ccd5f3bf3040e8c7b0c558b3dc7db0a134481ce
Maxime Pakula <pakulamaxime@gmail.com>
2022-01-13 08:53:52 +0100

    Suppression des fichier inutiles (tp google test, Bataille_navale.cpp).

--- TP-MEDEV/build_gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eleve/Documents/C++/TP-MEDEV/googletest/googlemock
-# Build directory: /home/eleve/Documents/C++/TP-MEDEV/build_gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- TP-MEDEV/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "lib.h"
-
-// entry point of the main project
-int main(int argc, char **argv)
-{
-    std::cout << add(5, 3) << std::endl;
-}
