c094a52bda2ad8f6233217713c0c70db43e2e683
Maxime Pakula <pakulamaxime@gmail.com>
2022-01-13 11:23:50 +0100

    Ajout du graphisme d'un tir qui touche.

--- Bataille_navale/Afficheur.h
+++ Bataille_navale/Afficheur.h
@@ -1,9 +1,17 @@
 #pragma once
 
+/// <summary>
+/// \class Afficheur
+/// \brief Permet l'affichage graphique du jeu à partir de l'état courant en mémoire
+/// </summary>
 class Afficheur {
 
 private :
+	int nbrCase;
+	float coorGrilleJoueur[4];
+	float coorGrilleOrdi[4];
 	void afficheGrille(float x_bas_gauche, float y_bas_gauche, float x_haut_droite, float y_haut_droite);
+	void afficheTouche(bool grilleActive, int noColonne, int noLigne);
 
 public :
 	Afficheur();
